f013ce593199aaaf06fed3c1e95c16b7ee0cabc8
Burtarn <robin.andersson0125@gmail.com>
2025-06-02 19:12:31 +0200

    Adderade paket för morgan, winston, logger osv.

---
+++ server/index.js
@@ -0,0 +1,46 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import morgan from 'morgan';
+import errorHandler from './middleware/errorHandler.js';
+
+import { swaggerUi, swaggerDocument } from './utils/swagger.js'; 
+
+//! Rutter
+import userRoutes from './routes/UserRoutes.js';
+import notesRoutes from './routes/notesRoutes.js';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+//! Middleware
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument)); 
+
+//! Rutter
+app.use('/api/user', userRoutes);   
+app.use('/api/notes', notesRoutes);  
+
+//! Health check
+app.get('/', (req, res) => {
+    res.send('Servern är igång!');
+});
+
+//! 404 fallback
+app.use((req, res) => {
+    res.status(404).json({ message: 'Endpoint not found' });
+});
+
+//! Global error handler 
+app.use(errorHandler);
+
+// Starta server
+app.listen(PORT, () => {
+    console.log(` Server is running on http://localhost:${PORT}`);
+    console.log(`Swagger docs: http://localhost:${PORT}/api-docs`);
+});
---
+++ server/services/notesServices.js
@@ -0,0 +1,34 @@
+    import pool from '../config/postgres.js';
+
+    export const getAllNotes = async (userId) => {
+    const result = await pool.query('SELECT * FROM notes WHERE user_id = $1', [userId]);
+    return result.rows;
+    };
+
+    export const createNote = async ({ title, content, userId }) => {
+    const result = await pool.query(
+        'INSERT INTO notes (title, content, user_id) VALUES ($1, $2, $3) RETURNING *',
+        [title, content, userId]
+    );
+    return result.rows[0];
+    };
+
+    export const updateNote = async ({ id, title, content }) => {
+    const result = await pool.query(
+        'UPDATE notes SET title = $1, content = $2 WHERE id = $3 RETURNING *',
+        [title, content, id]
+    );
+    return result.rows[0];
+    };
+
+    export const deleteNote = async (id) => {
+    await pool.query('DELETE FROM notes WHERE id = $1', [id]);
+    };
+
+    export const searchNotes = async (userId, query) => {
+    const result = await pool.query(
+        'SELECT * FROM notes WHERE user_id = $1 AND title ILIKE $2',
+        [userId, `%${query}%`]
+    );
+    return result.rows;
+    };
